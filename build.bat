e407277bc7c4a97dbf5aa3435f89484f4ef10752
jantari <jantari@github.com>
2020-10-28 16:09:08 +0100

    expand build script to show git info

--- build.bat
+++ build.bat
@@ -2,4 +2,16 @@
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
 
+:: Get last git commit
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --short HEAD`) DO (
+    SET LASTCOMMIT=%%F
+)
+
+:: Get dirty status of workspace
+FOR /F "tokens=* USEBACKQ" %%F IN (`git rev-parse --short HEAD`) DO (
+    SET LASTCOMMIT=%%F
+)
+
+ECHO Building:
+
 cl.exe no_children.cpp /EHsc
